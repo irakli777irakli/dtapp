94a8dc93e5ae31f707d9d2548ff43f6466de589d
ikako <ikako520@gmail.com>
2023-03-11 03:17:36 -0800

    end of section 6

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -21,6 +21,7 @@ export class AccountService {
         const user = response;
         if(user){
           localStorage.setItem('user',JSON.stringify(user));
+          console.log(user)
           this.currentUserSource.next(user);
         }
 
